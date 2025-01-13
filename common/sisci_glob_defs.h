af9ce4670eaad0baf6a2ed3a305a7da47c7ab8a3
Kristian Tjelta Johansen <kritjo@kritjo.com>
2025-01-13 17:37:18 +0100

    Helper header for various global definitions

---
+++ common/sisci_glob_defs.h
@@ -0,0 +1,29 @@
+#ifndef SISCI_PERF_SISCI_GLOB_DEFS_H
+#define SISCI_PERF_SISCI_GLOB_DEFS_H
+
+#define PUT_BROADCAST_GROUP_ID 1
+
+#define ADAPTER_NO 0
+
+#define NO_FLAGS 0
+#define NO_CALLBACK 0
+#define NO_ARG 0
+#define NO_OFFSET 0
+#define NO_SUGGESTED_ADDRESS 0
+
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+
+// SISCI Exit On Error -- SEOE
+#define SEOE(func, ...) \
+do {                    \
+    sci_error_t seoe_error; \
+    func(__VA_ARGS__, &seoe_error); \
+    if (seoe_error != SCI_ERR_OK) { \
+        fprintf(stderr, "%s failed: %s\n", #func, SCIGetErrorString(seoe_error)); \
+        exit(EXIT_FAILURE); \
+    } \
+} while (0)
+
+#endif //SISCI_PERF_SISCI_GLOB_DEFS_H
