f9addce7914faa429a442a7e12eccb0dc1633672
Kristian Tjelta Johansen <kritjo@kritjo.com>
2025-01-13 17:43:04 +0100

    SISCI init

--- client/main.c
+++ client/main.c
@@ -1,7 +1,20 @@
 #include "main.h"
-#include "stdlib.h"
+
+#include <stdlib.h>
+#include <sisci_api.h>
+
+#include "sisci_glob_defs.h"
 
 int main(int argc, char* argv[]) {
+    sci_desc_t sd;
+
+    SEOE(SCIInitialize, NO_FLAGS);
+    SEOE(SCIOpen, &sd, NO_FLAGS);
+
+
+
+    SEOE(SCIClose, sd, NO_FLAGS);
+    SCITerminate();
 
     return EXIT_SUCCESS;
 }
--- server/main.h
+++ server/main.h
@@ -1,4 +1,6 @@
 #ifndef DOUBLECLIQUE_MAIN_H
 #define DOUBLECLIQUE_MAIN_H
 
+int main(int argc, char* argv[]);
+
 #endif //DOUBLECLIQUE_MAIN_H
