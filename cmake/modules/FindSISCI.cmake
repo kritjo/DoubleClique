2e8e3a66bf1254eabf9bac7ceb1a0c524b31d8f0
Kristian Tjelta Johansen <kritjo@kritjo.com>
2025-01-15 15:11:37 +0100

    Changed to C17, C23 is too new and unsupported. Fixed include dirs for sisci.

--- cmake/modules/FindSISCI.cmake
+++ cmake/modules/FindSISCI.cmake
@@ -9,9 +9,12 @@ if(SISCI_LIB)
     # Create an imported library target
     add_library(SISCI STATIC IMPORTED)
 
+    list(APPEND SISCI_INCLUDE_DIRS "/opt/DIS/include")
+    list(APPEND SISCI_INCLUDE_DIRS "/opt/DIS/include/dis/")
+
     set_target_properties(SISCI PROPERTIES
             IMPORTED_LOCATION "${SISCI_LIB}"
-            INTERFACE_INCLUDE_DIRECTORIES "/opt/DIS/include"
+            INTERFACE_INCLUDE_DIRECTORIES "${SISCI_INCLUDE_DIRS}"
     )
 
     # Indicate to the parent that we found SISCI
