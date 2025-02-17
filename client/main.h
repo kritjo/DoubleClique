dc726fa6591283e975367ddb3092304d1daccaab
Kristian Tjelta Johansen <kritjo@kritjo.com>
2025-02-17 21:18:49 +0100

    Remove old code

--- client/main.h
+++ client/main.h
@@ -6,9 +6,6 @@
 #include <pthread.h>
 #include "put_request_region.h"
 #include "index_data_protocol.h"
-#include "sync_point.h"
-
-extern sync_point_t put_sync_point;
 
 int main(int argc, char* argv[]);
 
