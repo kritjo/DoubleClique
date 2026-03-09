60b1652411b8810fc8fe72ba2971a09a9c76b112
Kristian Tjelta Johansen <kristianj@dolphinics.com>
2026-03-09 13:37:40 +0100

    variant

--- common/avx_cpy.c
+++ common/avx_cpy.c
@@ -10,26 +10,26 @@ _memcpy_nt_avx2_helper(void *__restrict dst,
     unsigned char       *__restrict dest = (unsigned char *)dst;
     const unsigned char *__restrict s    = (const unsigned char *)src;
 
-    size_t i, vec_bytes, vec_end, prefix;
-    unsigned long misalign;
+    size_t i = 0, vec_bytes, vec_end, prefix;
+    uintptr_t misalign;
     int src_aligned;
     __m256i v0, v1;
 
-    /* Align dest to 64 bytes for stream stores */
-    misalign = (unsigned long)dest & 63UL;
-    prefix   = misalign ? (64UL - misalign) : 0UL;
+    /* Align dest to 64 bytes for streaming stores */
+    misalign = (uintptr_t)dest & 63u;
+    prefix   = misalign ? (64u - misalign) : 0u;
 
     if (prefix > bytes)
         prefix = bytes;
 
-    for (i = 0; i < prefix; ++i) {
+    for (; i < prefix; ++i) {
         dest[i] = s[i];
     }
 
-    vec_bytes = ((bytes - i) / 64UL) * 64UL;
+    vec_bytes = ((bytes - i) / 64u) * 64u;
     vec_end   = i + vec_bytes;
 
-    src_aligned = (((uintptr_t)(s + i)) & 31u) == 0;
+    src_aligned = ((((uintptr_t)(s + i)) & 31u) == 0);
 
     if (src_aligned) {
         for (; i < vec_end; i += 64) {
@@ -52,27 +52,30 @@ _memcpy_nt_avx2_helper(void *__restrict dst,
     }
 }
 
-void memcpy_nt_avx2(volatile void *__restrict dst,
+void memcpy_nt_avx2(void *__restrict dst,
                     const void *__restrict src,
                     size_t bytes,
-                    unsigned int chunk)
+                    size_t chunk)
 {
-    unsigned char *dst_tmp;
-    unsigned char *src_tmp;
-    size_t j;
+    unsigned char *dst_tmp = (unsigned char *)dst;
+    const unsigned char *src_tmp = (const unsigned char *)src;
 
-    if (bytes < (size_t)chunk * 2) {
-        _memcpy_nt_avx2_helper((void *__restrict) dst, src, bytes);
+    if (chunk == 0 || bytes == 0)
+        return;
+
+    if (bytes < chunk * 2) {
+        _memcpy_nt_avx2_helper(dst, src, bytes);
         _mm_sfence();
-    } else {
-        dst_tmp = (unsigned char *)dst;
-        src_tmp = (unsigned char *)src;
-
-        for (j = 0; j < bytes; j += chunk) {
-            _memcpy_nt_avx2_helper((void *__restrict) dst_tmp, src_tmp, chunk);
-            _mm_sfence();
-            dst_tmp += chunk;
-            src_tmp += chunk;
-        }
+        return;
     }
+
+    while (bytes != 0) {
+        size_t n = bytes < chunk ? bytes : chunk;
+        _memcpy_nt_avx2_helper(dst_tmp, src_tmp, n);
+        dst_tmp += n;
+        src_tmp += n;
+        bytes   -= n;
+    }
+
+    _mm_sfence();
 }
